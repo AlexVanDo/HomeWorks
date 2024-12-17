b67ea67e4cc77c858e8f4f8f97dcf8f5fcb48a83
VD <vandoker@list.ru>
2024-12-17 11:50:36 +0500

    New

--- Testing/tests/test_API_YD.py
+++ Testing/tests/test_API_YD.py
@@ -4,7 +4,7 @@ import requests
 class TestYDCreateFolder:
     def setup_method(self) -> None:
         self.headers = {
-            'Authorization': 'OAuth y0_AgAAAABnENF7AADLWwAAAADW0Zr0rLgkxmptRpqgS1mkh6kZ5279rAg'
+            'Authorization': ''
         }
 
     @pytest.mark.parametrize(
